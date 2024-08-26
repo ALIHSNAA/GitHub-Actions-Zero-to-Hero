350c23eaa1c12303bd4ab1115df4aa93815bbaf1
ALIHSNAA <48955251+ALIHSNAA@users.noreply.github.com>
2024-08-26 09:59:42 +0530

    Update addition.py
    sub function

--- src/addition.py
+++ src/addition.py
@@ -5,6 +5,11 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+def test_sub():
+    assert sub(7, 4) == 3
